623fb8e1721b641447cf2142abb662b79996abed
nickmagruder <nick.magruder@outlook.com>
2020-12-28 12:47:02 -0800

    trail api call & constructor built

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', getHomepage);
 app.post('/profile:id', getProfile);
 app.delete('/profile/delete', deleteTrail);
 app.put('/profile/update', updateTrail);
-app.post('/search', getSearches);
+app.get('/search', getSearches);
 app.post('/search/save', saveTrail);
 
 function getHomepage(req, res){
@@ -47,12 +47,77 @@ function updateTrail(req, res){
   //update trail information
 }
 
+
+
+
+// Seattle lat-long: 47.6038, -122.3300
 function getSearches(req, res){
-  //start by using default location of profile to render trails by calling api
   //Offer ability to input new location
+
+/*  const lat = req.query.latitude;
+    const long = req.query.longitude; */
+    const lat = 47.6038
+    const long = -122.3300
+    const TRAIL_API_KEY = process.env.TRAIL_API_KEY;
+    console.log(TRAIL_API_KEY);
+    const urlTrails = `http://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${long}&maxDistance=100&key=${TRAIL_API_KEY}&maxResults=10`;
+
+    return superagent.get(urlTrails)
+        .then(trailEntry => {
+            let trailsArray = trailEntry.body.trails;
+            let TrailData = trailsArray.map(trail => {
+                return new TrailConstructor(trail);
+            })
+            res.send(TrailData);
+            console.log(TrailData);
+        })
+        .catch(error => {
+            res.status(500).send('Sorry, an error has occured');
+            console.log(error, '500 Error')
+        });
+};
+
+
+function TrailConstructor(trailObject) {
+  this.trailID = trailObject.id;
+  this.lat = trailObject.latitude;
+  this.lon = trailObject.longitude;
+  this.trail = trailObject.name;
+  this.city = trailObject.location;
+  this.summary = trailObject.summary;
+  this.distance = trailObject.length;
+  this.rating = trailObject.stars;
+  this.elevation = trailObject.ascent;
+  this.difficulty = trailObject.difficulty;
+  this.trail_url = trailObject.url;
+  this.img_url = trailObject.imgMedium;
+/*   this.conditions = trailObject.conditionDetails;
+  this.condition_date = trailObject.conditionDate.slice(0, 10);
+  this.condition_time = trailObject.conditionDate.slice(11); */
 }
 
+// Copy of the schema:
+/* CREATE TABLE favorite(
+  ID SERIAL PRIMARY KEY,
+  username FOREIGN KEY,
+  completed VARCHAR(255),
+  date_completed DATE,
+  lat NUMBER,
+  lon NUMBER,
+  trail VARCHAR(255),
+  city VARCHAR(255),
+  summary TEXT,
+  distance NUMBER,
+  rating NUMBER,
+  elevation NUMBER,
+  difficulty VARCHAR(255),
+  trail_url VARCHAR(255),
+  img_url VARCHAR(255),
+  notes VARCHAR(255)
+); */
 
+
+// NICKTODO: user constructor
 
 
 app.use('*', (req, res) => res.status(404).send('Route you are looking for cannot be found'));
